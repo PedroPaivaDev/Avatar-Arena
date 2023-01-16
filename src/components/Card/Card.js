dcf7314328cdb91b8a2748b757cc50c872b8ca02
Pedro <pedropaivadev@gmail.com>
2023-01-16 20:45:03 -0300

    used the component card, instead of doing everything from scratch

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,13 @@
 import Input from 'components/Input/Input';
 import React from 'react';
 import styles from './Card.module.css';
+import cardFrame from 'assets/cardFace.png';
 
-const Card = ({card, id, disabled, action, setAction, ability, setAbility}) => {
+const Card = ({card, id, disabled, action, setAction, ability, setAbility, className}) => {
 
   return (
-    <div className={styles.cardFace} id={card.name}>
+    <div className={`${styles.cardFace} ${className}`} id={card.name}>
+      <img className={styles.cardFrame} src={cardFrame} alt='cardFace' />
       <h6 className={styles.subtitle}>{id}</h6>
       <h4 className={styles.title}>{card.name}</h4>
       <img className={styles.gif} src={card.image} alt={card.name}/>
--- src/components/CardSelector/CardFace.js
+++ src/components/CardSelector/CardFace.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from 'components/CardSelector/CardFace.module.css';
-import cardFrame from 'assets/cardFace.png';
 import Card from 'components/Card/Card';
 
 const CardFace = ({card, playerDeck, setPlayerDeck}) => {
@@ -21,9 +20,7 @@ const CardFace = ({card, playerDeck, setPlayerDeck}) => {
         onChange={handleChange}
       />
       <label htmlFor={`selector${card.name}`} className={styles.label} >
-        <h4 className={styles.name}>{card.name}</h4>
-        <img className={styles.cardFrame} src={cardFrame} alt='cardFace' />
-        <Card card={card} disabled={true}/>        
+        <Card card={card} disabled={true} className={styles.card}/>        
       </label>
     </div>
   )
