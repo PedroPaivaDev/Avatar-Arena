455e42af5ee6c4e0aed178f0e294e5e1c8cd9b79
Pedro <pedropaivadev@gmail.com>
2023-02-06 10:17:06 -0300

    resetGame method to clear localStorage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './Header.module.css';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
 
   const {pathname} = useLocation();
+  const navigate = useNavigate();
+
+  function resetGame() {
+    if(window.confirm('Tem certeza que deseja perder todo o jogo?')){
+      localStorage.clear();
+      navigate('/');
+    } else return
+  }
 
   function handleGameButton() {
     if(pathname==='/game') {
-      return <button className={styles.game}>Reset</button>
+      return <button className={styles.game} onClick={resetGame}>Recomeçar</button>
     } else {
       return <Link className={styles.game} to='game'>Jogar</Link>
     }
